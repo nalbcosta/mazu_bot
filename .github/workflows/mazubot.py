77d6ddf3bb57b5d9deab35582895db63bf6c8356
Nalbert Schwank Costa Santos <100242852+nalbcosta@users.noreply.github.com>
2024-07-09 12:30:32 -0300

    Update mazubot.py

--- .github/workflows/mazubot.py
+++ .github/workflows/mazubot.py
@@ -5,7 +5,8 @@ import asyncio
 import schedule
 import time
 import logging
-from aiogram import Bot
+import os
+from aiogram import Bot, Dispatcher, types
 
 #Mensagens Automáticas
 msg_1 = '🚀- Teste Amazon Prime por 1 MES de graça: https://amzn.to/3XONnME \n📖- Teste Audible Library por 3 MESES de graça: https://amzn.to/3XVozmx \n🎧- Teste Amazon Music Unlimited por 5 MESES de graça: https://amzn.to/4cNtfiw'
@@ -14,9 +15,13 @@ msg_1 = '🚀- Teste Amazon Prime por 1 MES de graça: https://amzn.to/3XONnME \
 img_1 = 'https://i.pcmag.com/imagery/articles/05qp7E8Z6G2lM79Y6Epl0tl-11.jpg'
 
 #Script
+telegram_token = os.getenv("TELEGRAM_TOKEN")
+if not telegram_token:
+    raise ValueError("Bot token not set in environment variables")
+    
 async def send_message_prime():
-    bot_token = '6998561912:AAGOicoQuDHnJHY8NcLhEQKItLznmC_GrDE'
-    bot = Bot(token=bot_token)
+    bot = Bot(token=telegram_token)
+    dp = Dispatcher(bot)
     channel_username = '@mazuofertas'
 
     await bot.send_photo(chat_id=channel_username, photo=img_1)
